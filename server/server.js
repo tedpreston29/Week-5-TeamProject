8b520563be99137422e6a2cfce8f3d3ee6bf5c3b
Ted Preston <tedpreston29@gmail.com>
2025-09-29 14:40:20 +0100

    boilerplate added

---
+++ server/server.js
@@ -0,0 +1,22 @@
+import express from "express";
+import cors from "cors";
+import pg from "pg";
+import dotenv from "dotenv";
+
+const app = express();
+app.use(express.json());
+dotenv.config();
+app.use(cors());
+
+const db = new pg.Pool({
+    conntectionString: process.env.
+})
+
+
+app.get('/', (req, res) => {
+    res.status(200).json(`This is OUR root route`)
+})
+
+app.listen(7878, ()=> {
+    console.log(`server running on http://localhost:7878`)
+})
