42b2ddac6acf710f6f04b8b111b967c0c2e76eaa
tris43210 <tristan.g.king@gmail.com>
2025-09-30 11:32:36 +0100

    added buttons consts and fixed server get route to db

--- client/src/main.js
+++ client/src/main.js
@@ -1,11 +1,17 @@
-const buttonmood = document.getElementById(``);
-const buttonmood = document.getElementById(``);
-const buttonmood = document.getElementById(``);
-const buttonmood = document.getElementById(``);
-const buttonmood = document.getElementById(``);
+const buttonMoodHappy = document.getElementById(`happy`);
+const buttonMoodSad = document.getElementById(`sad`);
+const buttonMoodAdven = document.getElementById(`adventurous`);
+const buttonMoodFright = document.getElementById(`frightening`);
+const buttonMoodThrill = document.getElementById(`thrilling`);
+
+const moodContainer = document.getElementById(`mainDisplay`); 
 
 async function getMoodData() {
   const getData = await fetch();
 
   console.log(getData);
 }
+
+function generateHTML
+
+
